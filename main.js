2685fe10bceb822947a0bb1418a578f77df3dd2f
WojtekU <voytecu@gmail.com>
2021-12-22 21:25:49 +0100

    Dirty code many repeats but looks like its working with JS+10 on good answer

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const fields = document.querySelectorAll('.fields');
-let playersPoints = [], playerFieldBefore = [], names = [], round=1, nameCounter=0, namePrompt;
+let playersPoints = [], playerFieldBefore = [], names = [], round=1, nameCounter=0, namePrompt, beatenColor, beatenPlayerNumber;
 let color = ["red","blue","green","orange","yellow","lightgrey","pink","cadetblue","darkgoldenrod","rebeccapurple","aqua","burlywood","coral","darkmagenta","firebrick","indigo","khaki","lavender","maroon","midnightblue"];
-const changDirectionToLeft = [-9,9,7,5,3,1,-1,-3,-5,-7];
+const changDirectionToLeft = [-9,9,7,5,3,1,-1,-3,-5,-7,-9];
 
-while(namePrompt!=="START"){
+while(namePrompt!=="START" || nameCounter>color.length){
 namePrompt = prompt("Enter player name or type START to begin the game","START");
 names.push(namePrompt);
 nameCounter++;}
@@ -24,12 +24,14 @@ for(let i=10; i<92;i+=20){
 }
 /*SET NUMBERS ON BOARD*/
 
-function moveAuto(playerPoints,playerNumber) {
+function throwDice(playerPoints,playerNumber) {
 
-    let playerField, playerDirection, beatenColor, beatenPlayerNumber;
-    let random = Math.ceil(Math.random()*6);
+    let playerField, playerDirection;
+    let random = Math.ceil(Math.random()*6); //dice
+    
+    
     playersPoints[playerNumber]+=random; 
-
+    if( playersPoints[playerNumber] >=100) {alert(`Player ${playerNumber+1} ` + "WINS!")}
     console.log(`Player number ${playerNumber+1} got ${random}, his pts is ${playersPoints[playerNumber]} in round ${round}`);
     
     // playerDirection = (Math.floor(playersPoints[playerNumber]-1/10)%2) ? "left" : "right";
@@ -54,17 +56,12 @@ function moveAuto(playerPoints,playerNumber) {
     playerField = (playerDirection==="right") ? playersPoints[playerNumber] : playersPoints[playerNumber] + changDirectionToLeft[playersPoints[playerNumber]%10];
 
 
-    if( playersPoints[playerNumber] >=100) {alert(`Player ${playerNumber+1} ` + "WINS!")}
-
-
     if(fields[playerFieldBefore[playerNumber]].childNodes[2].style.color===color[playerNumber]) {
         fields[playerFieldBefore[playerNumber]].childNodes[2].style.visibility="hidden";
         fields[playerFieldBefore[playerNumber]].childNodes[4].style.visibility="hidden";
     }
 
-    // putPlayer();
     
-    // function putPlayer(){
     if(fields[playerField-1].childNodes[2].style.visibility==="visible"){
         beatenColor = fields[playerField-1].childNodes[2].style.color;
         for(let i=0; i<playersPoints.length; i++){
@@ -80,41 +77,65 @@ function moveAuto(playerPoints,playerNumber) {
     fields[playerField-1].childNodes[4].style.visibility="visible";
         
     playerFieldBefore[playerNumber] = playerField-1;
-//         else {
-//             playersPoints[playerNumber]++; 
-//             switch(playerField) {
-//                 case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9:  
-//                 case 22: case 23: case 24: case 25: case 26: case 27: case 28: case 29: 
-//                 case 42: case 43: case 44: case 45: case 46: case 47: case 48: case 49: 
-//                 case 62: case 63: case 64: case 65: case 66: case 67: case 68: case 69: 
-//                 case 82: case 83: case 84: case 85: case 86: case 87: case 88: case 89: 
-//                 case 21: case 41: case 61: case 81: 
-//                     playerField++; break;
-//                 case 10: case 30: case 50: case 70: case 90:
-//                 case 11: case 31: case 51: case 71:
-//                     playerField+=10; break;
-//                 case 12: case 13: case 14: case 15: case 16: case 17: case 18: case 19: 
-//                 case 32: case 33: case 34: case 35: case 36: case 37: case 38: case 39: 
-//                 case 52: case 53: case 54: case 55: case 56: case 57: case 58: case 59: 
-//                 case 72: case 73: case 74: case 75: case 76: case 77: case 78: case 79: 
-//                 case 92: case 93: case 94: case 95: case 96: case 97: case 98: case 99: 
-//                 case 20: case 40: case 60: case 80: case 100:
-//                     playerField--; break;
-//                 case 91: alert(`Player ${playerNumber+1}: ${names[playerNumber]} wins!`); break;
-//             }
-//             putPlayer();}
     
-//     }
+    jsQuestion(playerNumber,playerDirection);
 
  }
+
+/*After dice comes JS answers mechanism*/
+
+function jsQuestion(playerNumber,playerDirection) {
+
+    fields[playerFieldBefore[playerNumber]].childNodes[2].style.visibility="hidden";
+    fields[playerFieldBefore[playerNumber]].childNodes[4].style.visibility="hidden"; console.log(playerFieldBefore[playerNumber]);
+
+    if(Math.round(Math.random()*3)>0) { alert("JS OK");
+        
+        if(fields[playerFieldBefore[playerNumber]+10].childNodes[2].style.visibility==="visible"){
+            beatenColor = fields[playerFieldBefore[playerNumber]+10].childNodes[2].style.color;
+            for(let i=0; i<playersPoints.length; i++){
+                if(beatenColor===color[i]) {beatenPlayerNumber=i; break;}
+            }
+
+            fields[playerFieldBefore[playerNumber]].childNodes[2].style.color=color[beatenPlayerNumber];
+            fields[playerFieldBefore[playerNumber]].childNodes[4].style.color=color[beatenPlayerNumber];
+            fields[playerFieldBefore[playerNumber]].childNodes[4].innerText=names[beatenPlayerNumber];
+            fields[playerFieldBefore[playerNumber]].childNodes[2].style.visibility="visible";
+            fields[playerFieldBefore[playerNumber]].childNodes[4].style.visibility="visible";
+
+            playersPoints[beatenPlayerNumber] = (playerDirection==="right") ? playerFieldBefore[playerNumber] + 1 : playerFieldBefore[playerNumber] + 1 -  changDirectionToLeft[(playerFieldBefore[playerNumber]+1)%10]; //??
+
+            playerFieldBefore[beatenPlayerNumber] = playerFieldBefore[playerNumber];
+        }
+        
+        fields[playerFieldBefore[playerNumber]+10].childNodes[2].style.color=color[playerNumber];
+        fields[playerFieldBefore[playerNumber]+10].childNodes[4].style.color=color[playerNumber];
+        fields[playerFieldBefore[playerNumber]+10].childNodes[4].innerText=names[playerNumber];
+        fields[playerFieldBefore[playerNumber]+10].childNodes[2].style.visibility="visible";
+        fields[playerFieldBefore[playerNumber]+10].childNodes[4].style.visibility="visible";
+
+        playersPoints[playerNumber] = (playerDirection==="right") ? playerFieldBefore[playerNumber] + 11 +  changDirectionToLeft[((playerFieldBefore[playerNumber]+1)%10)] : playerFieldBefore[playerNumber] + 11 ;
+
+        playerFieldBefore[playerNumber] = playerFieldBefore[playerNumber]+10;
+        if( playersPoints[playerNumber] >=100) {alert(`Player ${playerNumber+1} ` + "WINS!")}
+    
+    } else {alert("JS NO OK");
+        fields[playerFieldBefore[playerNumber]].childNodes[2].style.visibility="visible";
+        fields[playerFieldBefore[playerNumber]].childNodes[4].style.visibility="visible";
+
+            }
+
+
+}
+/***************************************/
 
 for(let j=0; j<20; j++) {
     for(let i=0; i<playersPoints.length; i++){
         // alert(`NEXT MOVE ${names[i]}`);
-        moveAuto(playersPoints[i],i)
+        throwDice(playersPoints[i],i)
     }
     round++;
     console.log(playersPoints);
 }
 
-// setInterval(moveAuto,600,playersPoints[0],0);
+// setInterval(throwDice,600,playersPoints[0],0);
