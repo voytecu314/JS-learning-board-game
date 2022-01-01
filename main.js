b24daa8805edd73171cf48991da4303fe960d139
WojtekU <voytecu@gmail.com>
2022-01-01 23:46:48 +0100

    'Extra throw or question after correct answer rule' added --> Bugs fixed: playerDirection was not correct on second JS correct answer -setDirection function is set

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const fields = document.querySelectorAll('.fields');
 const dice = document.getElementById('dice-canvas');
 const playerBoard = document.querySelectorAll('.player-board'); console.log(playerBoard);
 
-let playersPoints = [], playersPointsHistory = [], playerFieldBefore = [], names = [], round=0, nameCounter=0, namePrompt, beatenColor, beatenPlayerNumber, randomBefore=6, secondThrow, secondQuestion;
+let playersPoints = [], playersPointsHistory = [], playerFieldBefore = [], names = [], round=0, nameCounter=0,playerDirection, namePrompt, beatenColor, beatenPlayerNumber, randomBefore=6, secondThrow, secondQuestion;
 let color = ["red","blue","green","orange","yellow","lightgrey","pink","cadetblue","darkgoldenrod","rebeccapurple","aqua","burlywood","coral","darkmagenta","firebrick","indigo","khaki","lavender","maroon","midnightblue"];
 
 const changDirectionToLeft = [-9,9,7,5,3,1,-1,-3,-5,-7,-9];
@@ -48,6 +48,28 @@ for(let i=0; i<names.length-1; i++){
 
 console.log(names);
 
+function setDirection(playerPts) {
+                
+            // playerDirection = (Math.floor(playersPoints[playerNumber]-1/10)%2) ? "left" : "right";
+
+            switch(playerPts) {
+                case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10: 
+                case 21: case 22: case 23: case 24: case 25: case 26: case 27: case 28: case 29: case 30:
+                case 41: case 42: case 43: case 44: case 45: case 46: case 47: case 48: case 49: case 50:
+                case 61: case 62: case 63: case 64: case 65: case 66: case 67: case 68: case 69: case 70:
+                case 81: case 82: case 83: case 84: case 85: case 86: case 87: case 88: case 89: case 90:
+                    playerDirection = "right"; break;
+                case 11: case 12: case 13: case 14: case 15: case 16: case 17: case 18: case 19: case 20: 
+                case 31: case 32: case 33: case 34: case 35: case 36: case 37: case 38: case 39: case 40:
+                case 51: case 52: case 53: case 54: case 55: case 56: case 57: case 58: case 59: case 60:
+                case 71: case 72: case 73: case 74: case 75: case 76: case 77: case 78: case 79: case 80:
+                case 91: case 92: case 93: case 94: case 95: case 96: case 97: case 98: case 99: case 100:
+                    playerDirection = "left"; break;
+            }
+
+            return playerDirection;
+}
+
 function setField(fieldNo, playerNo) {
     fields[fieldNo].childNodes[2].style.color=color[playerNo];
     fields[fieldNo].childNodes[4].style.color=color[playerNo];
@@ -81,7 +103,7 @@ function drawChart(playerNo) {
     let tempRoundArray = [];
     let move = 0, chartRoundFraction, fontPx;
 
-    chartRoundFraction = (round<10)?30:(round<=12)?24:(round<=16)?18:15;
+    chartRoundFraction = (round<6)?36:(round<10)?30:(round<=12)?24:(round<=16)?18:15;
 
     if(playerNo==0) {
         fontPx = (round<=10)?"8px":(round<=16)?"6px":"4px";
@@ -141,13 +163,13 @@ fetch("./quiz.json")
 
             displayRound(playerNumber);
 
-            alert(`Please throw the dice player-${playerNumber+1} (${names[playerNumber]})`);
+            alert(`Please throw the dice ${names[playerNumber]}`);
             
             if(playerPoints===0) {
                 playerBoard[playerNumber].style.display='none';
             }
 
-            let playerField, playerDirection, diceRotationY, diceRotationX, repeatRotationCounter=0;
+            let playerField, diceRotationY, diceRotationX, repeatRotationCounter=0;
             let random = Math.ceil(Math.random()*6); //dice
 
             switch(random) {
@@ -163,7 +185,7 @@ fetch("./quiz.json")
                 repeatRotationCounter++;
                 if(repeatRotationCounter%2==0) {
                     dice.style.transform=`rotateY(${diceRotationY+360}deg) rotateX(${diceRotationX+360}deg)`;
-                    }else {
+                    } else {
                     dice.style.transform=`rotateY(${diceRotationY-360}deg) rotateX(${diceRotationX-360}deg)`;
                     }
                 
@@ -179,25 +201,8 @@ fetch("./quiz.json")
             playersPointsHistory[round][playerNumber].push(playersPoints[playerNumber]);
             
             console.log(`${names[playerNumber]} got ${random}, his pts is ${playersPoints[playerNumber]} in round ${round+1}`);
-            
-            // playerDirection = (Math.floor(playersPoints[playerNumber]-1/10)%2) ? "left" : "right";
 
-            switch(playersPoints[playerNumber]) {
-                case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10: 
-                case 21: case 22: case 23: case 24: case 25: case 26: case 27: case 28: case 29: case 30:
-                case 41: case 42: case 43: case 44: case 45: case 46: case 47: case 48: case 49: case 50:
-                case 61: case 62: case 63: case 64: case 65: case 66: case 67: case 68: case 69: case 70:
-                case 81: case 82: case 83: case 84: case 85: case 86: case 87: case 88: case 89: case 90:
-                    playerDirection = "right"; break;
-                case 11: case 12: case 13: case 14: case 15: case 16: case 17: case 18: case 19: case 20: 
-                case 31: case 32: case 33: case 34: case 35: case 36: case 37: case 38: case 39: case 40:
-                case 51: case 52: case 53: case 54: case 55: case 56: case 57: case 58: case 59: case 60:
-                case 71: case 72: case 73: case 74: case 75: case 76: case 77: case 78: case 79: case 80:
-                case 91: case 92: case 93: case 94: case 95: case 96: case 97: case 98: case 99: case 100:
-                    playerDirection = "left"; break;
-
-            }
-            
+            playerDirection = setDirection(playersPoints[playerNumber]);
 
             playerField = (playerDirection==="right") ? playersPoints[playerNumber] : playersPoints[playerNumber] + changDirectionToLeft[playersPoints[playerNumber]%10];
 
@@ -219,13 +224,15 @@ fetch("./quiz.json")
                 playerBoard[beatenPlayerNumber].style.display='flex';
             }
 
-            setField(playerField-1, playerNumber)
+            setField(playerField-1, playerNumber);
                 
             playerFieldBefore[playerNumber] = playerField-1;
             
             console.log(playersPoints);
-
-            if(!secondThrow) jsQuestion(playerNumber,playerDirection);
+            if(random==6) {
+                alert('Lucky! You got 6, you will throw the dice again.')
+                throwDice(playerPoints,playerNumber);
+            } else if(!secondThrow) jsQuestion(playerNumber,playerDirection);
         }
 
         /*After dice comes JS answers mechanism*/
@@ -246,13 +253,11 @@ fetch("./quiz.json")
                 fields[playerFieldBefore[playerNumber]].childNodes[4].style.visibility="hidden"; 
 
                 alert("Very good answer!\n\nBONUS: Jump to next row.");
-
+                console.log(playerDirection+":"+playerFieldBefore[playerNumber]);
                 playersPoints[playerNumber] = (playerDirection==="right") ? playerFieldBefore[playerNumber] + 11 +  changDirectionToLeft[((playerFieldBefore[playerNumber]+1)%10)] : playerFieldBefore[playerNumber] + 11 ;
                 playersPointsHistory[round][playerNumber].push(playersPoints[playerNumber]);
-
+                console.log(playerDirection+":"+playerFieldBefore[playerNumber]);
                 if( playerFieldBefore[playerNumber]+10 >=100) winner(playerNumber);
-
-                console.log(`${names[playerNumber]} got bonus \n ${playersPoints}`);
 
                 if(fields[playerFieldBefore[playerNumber]+10] && fields[playerFieldBefore[playerNumber]+10].childNodes[2].style.visibility==="visible"){
                     beatenColor = fields[playerFieldBefore[playerNumber]+10].childNodes[2].style.color;
@@ -272,6 +277,8 @@ fetch("./quiz.json")
 
                 playerFieldBefore[playerNumber] = playerFieldBefore[playerNumber]+10;
 
+                console.log(`${names[playerNumber]} got bonus \n ${playersPoints}`);
+
                 secondThrow = !secondThrow;
 
                 if(secondThrow){
@@ -281,7 +288,10 @@ fetch("./quiz.json")
                     }
                     if(secondQuestion.toUpperCase()==="T"){
                         throwDice(playersPoints[playerNumber],playerNumber);
-                    } else jsQuestion(playerNumber,playerDirection);
+                    } else {
+                        playerDirection = setDirection(playersPoints[playerNumber]);
+                        jsQuestion(playerNumber,playerDirection);
+                        }
                 }
             
             } else {alert("Answer not correct, good luck next time");
